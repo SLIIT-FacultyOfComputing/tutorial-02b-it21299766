8761c38526a93f454b196446de4f30f5a50a4dc1
it21299766 <99312863+it21299766@users.noreply.github.com>
2022-02-17 15:54:59 +0000

    completed

--- tute04.cpp
+++ tute04.cpp
@@ -18,12 +18,34 @@ long nCr(int n, int r);
 
 int main() {
   int n, r;
-  std::cout << "Enter a value for n ";
+  std::cout << "Enter a value for n : ";
   std::cin >> n;
-  std::cout << "Enter a value for r ";
+  std::cout << "Enter a value for r :";
   std::cin >> r;
   std::cout << "nCr = ";
   std::cout << nCr(n,r);
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int no)
+{  
+  long fac;
+  int i;
+  fac = 1;
+
+  for(i=no;i>=1;i--)
+    {
+      fac =fac*i;
+      
+    }
+  return fac;
+  }
+long nCr(int n,int r)
+{
+  long nCr;
+  nCr=Factorial(n)/(Factorial(r)*Factorial(n-r));
+  return nCr;
+}
+  
+  
+  
